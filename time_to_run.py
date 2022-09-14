6b6eaf405c66f9642448a684d0c549f3c63752e1
sshivanshu992 <sshivanshu902@gmail.com>
2022-09-14 09:32:53 +0530

    adding file for test

---
+++ time_to_run.py
@@ -0,0 +1,11 @@
+from datetime import datetime as dt
+
+RUN = True
+
+while RUN:
+	if dt(dt.now().year,dt.now().month,dt.now().day,18,30) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,18,34):
+		print("Working")
+	else:
+		RUN = False
+		print("Done!!!")
+
